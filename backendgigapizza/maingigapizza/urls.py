23c82b26acaf420c21e42908df6fe60e09188f6d
Lucas Soares <soareslukas9090@gmail.com>
2023-11-28 09:25:01 -0300

    Recriação do projeto

---
+++ backendgigapizza/maingigapizza/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    ###### CATEGORIAS ######
+    path('cadastrar_categoria/', cadastrarCategoria, name='cadastrar_categoria'),
+    path('editar_categoria/', editarCategoria, name='editar_categoria'),
+    path('inativar_categoria/', inativarCategoria, name='inativar_categoria'),
+    path('ativar_categoria/', ativarCategoria, name='ativar_categoria'),
+    path('listar_categorias/', listarCategorias, name='listar_categorias'),
+    path('listar_categoria/', listarCategoria, name='listar_categoria'),
+    ###### SUBCATEGORIAS ######
+    path('cadastrar_subcategoria/', cadastrarSubcategoria, name='cadastrar_subcategoria'),
+    path('editar_subcategoria/', editarSubcategoria, name='editar_sucategoria'),
+    path('inativar_subcategoria/', inativarSubcategoria, name='inativar_sucategoria'),
+    path('ativar_subcategoria/', ativarSubcategoria, name='ativar_sucategoria'),
+    path('listar_subcategorias/', listarSubcategorias, name='listar_sucategorias'),
+    path('listar_subcategoria/', listarSubcategoria, name='listar_sucategoria'),
+]
