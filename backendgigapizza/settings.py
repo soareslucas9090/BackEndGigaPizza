529a8b2a1df3c59a13429e4baa8ed0eda01a33aa
Lucas Soares <soareslukas9090@gmail.com>
2023-12-21 00:09:21 -0300

    toGCloud

--- backendgigapizza/settings.py
+++ backendgigapizza/settings.py
@@ -41,10 +41,10 @@ else:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
