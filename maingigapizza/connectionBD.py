9f254cbfc3a082fc4fdee33e7e8ba537259c7d5e
eniocharles <eniocharles15@gmail.com>
2024-01-05 09:36:29 -0300

    Update connectionBD.py

--- maingigapizza/connectionBD.py
+++ maingigapizza/connectionBD.py
@@ -292,56 +292,163 @@ def listar_item_venda(id_item_venda):
 
 
 def listar_itens_pedido(id_pedido):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_itens_pedido(%s)", [id_pedido])
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
-def criar_pedido(
-    hora_entrega, descricao_pedido, id_usuario_requisitante, id_usuario_pedido
-):
-    ...
+def criar_pedido(hora_entrega, descricao_pedido, id_usuario_requisitante, id_usuario_pedido):
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from criar_pedido(%s, %s, %s, %s)",
+            [
+                hora_entrega,
+                descricao_pedido,
+                id_usuario_requisitante,
+                id_usuario_pedido
+            ],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
 
 
-def editar_pedido(
-    id_pedido, hora_entrega, nova_descricao_pedido, id_usuario_requisitante
-):
-    ...
+def editar_pedido(id_pedido, hora_entrega, nova_descricao_pedido, id_usuario_requisitante):
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from editar_pedido(%s, %s, %s, %s)",
+            [id_pedido, hora_entrega, nova_descricao_pedido, id_usuario_requisitante],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
 
 
 def finalizar_pedido(id_pedido):
-    ...
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * from finalizar_pedido()")
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
 def listar_pedido():
-    ...
-
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_pedido()")
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 def listar_pedido(pedido_id):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_pedido(%s)", [pedido_id])
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
 def listar_pedido_cliente(cliente_id):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_pedido_cliente(%s)", [cliente_id])
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
 def criar_pizza(id_item_venda, tamanho_pizza):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from criar_pizza(%s, %s)",
+            [
+                id_item_venda, 
+                tamanho_pizza
+            ],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
 
 
 def listar_pizza():
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_pizza()")
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
 def listar_pizzas_pedido(id_pedido_busca):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute("SELECT * from listar_pizzas_pedido(%s)", [id_pedido_busca])
+        resultado = cursor.fetchall()
+        cursor.close()
+        return resultado
 
 
 def criar_sabor_pizza(pizza_id, itemvenda_id):
-    ...
-
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from criar_sabor_pizza(%s, %s)",
+            [
+                pizza_id, 
+                itemvenda_id
+            ],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
 
 def criar_pizzapedido(pedido_id, pizza_id):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from criar_pizzapedido(%s, %s)",
+            [
+                pedido_id, 
+                pizza_id
+            ],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
 
 
 def criar_itempedido(valor, quantidade, item_venda_id, pedido_id):
-    ...
+    with connection.cursor() as cursor:
+        # Aqui chamo a função criada no banco
+        cursor.execute(
+            "SELECT * from criar_itempedido(%s, %s, %s, %s)",
+            [
+                valor, 
+                quantidade, 
+                item_venda_id, 
+                pedido_id
+            ],
+        )
+        resultado = cursor.fetchall()
+        # O resultado é um vetor dentro de um vetor, por isso o [0][0]
+        resultado = resultado[0][0]
+        cursor.close()
+        return resultado
